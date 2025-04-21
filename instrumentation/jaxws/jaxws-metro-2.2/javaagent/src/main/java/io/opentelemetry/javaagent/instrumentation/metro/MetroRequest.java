0e0f237bcd8eb4b24d7d90574f140091f4d0170d
bohat <work.puttipong@gmail.com>
2025-04-21 10:10:38 +0700

    add error handling when get oper failed

--- instrumentation/jaxws/jaxws-metro-2.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/metro/MetroRequest.java
+++ instrumentation/jaxws/jaxws-metro-2.2/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/metro/MetroRequest.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.javaagent.instrumentation.metro;
 
+import com.sun.xml.ws.api.message.Message;
 import com.sun.xml.ws.api.message.Packet;
 import com.sun.xml.ws.api.server.WSEndpoint;
 
@@ -24,10 +25,31 @@ public class MetroRequest {
   public Packet packet() {
     return packet;
   }
+  private static String getOperationNameFrom(Packet packet){
+    String operationName = "NO_OPERATION";
+    try{
+      Message request = packet.getMessage();
+      String localport = request.getPayloadLocalPart();
+      operationName = localport.replace("Datarequestparam", "");
+      if(localport == null){
+        String nsUri = request.getPayloadNamespaceURI();
+        operationName = nsUri;
+      }
+      return operationName;
+    }catch (Throwable e){
+      return "Failed-getPayloadLocalPart";
+    }
 
+  }
   private static String getSpanName(WSEndpoint<?> endpoint, Packet packet) {
-    String serviceName = endpoint.getServiceName().getLocalPart();
-    String operationName = packet.getWSDLOperation().getLocalPart();
+    String serviceName = "NO_NAME";
+    String operationName = "NO_OPERATION";
+    try{
+      serviceName = endpoint.getServiceName().getLocalPart();
+      operationName = packet.getWSDLOperation().getLocalPart();
+    }catch(Throwable e){
+      operationName = getOperationNameFrom(packet);
+    }
     return serviceName + "/" + operationName;
   }
 }
